ae7476b3a179f9f6b2038b8d6a685fa39c81d556
millsi <mills.i.s@live.com>
2014-03-16 17:38:39 -0700

    Updates to Player Object
    defines object "Player" that can be initialized in other classes such as
    game control. you could have more than one at some point based on my
    code

---
+++ WumpusTest/WumpusTest/Player.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WumpusTest
+{
+    partial class playerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getStatusButton = new System.Windows.Forms.Button();
+            this.statusText = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // getStatusButton
+            // 
+            this.getStatusButton.Location = new System.Drawing.Point(12, 12);
+            this.getStatusButton.Name = "getStatusButton";
+            this.getStatusButton.Size = new System.Drawing.Size(160, 34);
+            this.getStatusButton.TabIndex = 11;
+            this.getStatusButton.Text = "Get Status";
+            this.getStatusButton.UseVisualStyleBackColor = true;
+            this.getStatusButton.Click += new System.EventHandler(this.getStatusButton_Click);
+            // 
+            // statusText
+            // 
+            this.statusText.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.statusText.Location = new System.Drawing.Point(12, 52);
+            this.statusText.Multiline = true;
+            this.statusText.Name = "statusText";
+            this.statusText.ReadOnly = true;
+            this.statusText.Size = new System.Drawing.Size(160, 74);
+            this.statusText.TabIndex = 1;
+            // 
+            // playerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(186, 139);
+            this.Controls.Add(this.getStatusButton);
+            this.Controls.Add(this.statusText);
+            this.Name = "playerForm";
+            this.Text = "Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox statusText;
+        private System.Windows.Forms.Button getStatusButton;
+    }
+}
--- WumpusTest/WumpusTest/Player.cs
+++ WumpusTest/WumpusTest/Player.cs
@@ -1,16 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WumpusTest
 {
-    class Player
+    public partial class playerForm : Form
     {
-        public Player()
+        private Player p1 = new Player();
+        public playerForm()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void getStatusButton_Click(object sender, EventArgs e)
+        {
+            statusText.Text = p1.toString();
+        }
+    }
+    public class Player
+    {
+        #region variables
+        private int roomNumber;
+        private int arrows;
+        private int gold;
+        private int score;
+        private String name;
+        private int[] connectingRooms= new int[3]; 
+        #endregion
+        public Player()
+        {
+            name = "Player 1";
+            roomNumber = -1;
+            arrows = 0;
+            gold = 0;
+            score = 0;
+        }
+        public Player(String name)
+        {
+            this.name = name;
+            roomNumber = -1;
+            arrows = 0;
+            gold = 0;
+            score = 0;
+        }
+        public int getRoomNumber()
+        {
+            return roomNumber;
+        }
+        public int getArrows()
+        {
+            return arrows;
+        }
+        public int getGold()
+        {
+            return gold;
+        }
+        public int getScore()
+        {
+            return score;
+        }
+        public void setRoomNumber(int room)
+        {
+            roomNumber = room;
+        }
+        public void setArrows(int arrows)
+        {
+            this.arrows = arrows;
+        }
+        public void buyArrow()
+        {
+            if(gold>=5){
+                arrows++;
+                gold-=5;
+            }
+        }
+        public void setGold(int gold)
+        {
+            this.gold = gold;
+        }
+        public void earnGold(int earned){
+            gold += earned;
+        }
+        public void updateScore(int earned)
+        {
+            score += earned;
+        }
+        public void setScore(int score)
+        {
+            this.score = score;
+        }
+        public void getConnectingRooms(int[] rooms)
+        {
+            connectingRooms = rooms;
+        }
+        public String toString()
+        {
+            String str = "";
+            str += "Name: " + name + Environment.NewLine;
+            str += "Current Room: " + roomNumber + Environment.NewLine;
+            str += "Arrows: " + arrows + Environment.NewLine;
+            str += "Gold: " + gold + Environment.NewLine;
+            str += "Score: " + score + Environment.NewLine;
+            return str;
         }
     }
 }
